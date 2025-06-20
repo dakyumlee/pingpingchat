d5fd90fe38631335e7261b7e4a3f12d433049629
dakyumlee <oicrcutie@gmail.com>
2025-06-20 14:24:22 +0900

    fix model name

--- api/pingping.js
+++ api/pingping.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         "anthropic-version": "2023-06-01",
       },
       body: JSON.stringify({
-        model: "claude-3.5",
+        model: "claude-3-5-sonnet-20240620", 
         system,                   
         messages,                   
         max_tokens_to_sample: 1024, 
